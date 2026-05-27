1106e3085838e5b84b6cae26ff560b278e1d5e67
ti2140 <ti.tube140@gmail.com>
2026-05-27 17:23:17 +0900

    main.tf fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,25 +30,4 @@ provider "snowflake" {
   private_key       = var.snowflake_private_key
 }
 
-# ==========================================
-# 3. DB定義（SYSADMINで実行）
-# ==========================================
-resource "snowflake_database" "training_db" {
-  name    = "${upper(var.trainee_name)}_TRAINING_DB"
-  comment = "Training DB for ${var.trainee_name}. Created by Terraform."
-}
-
-# ==========================================
-# 4. DBT用ロールへの権限付与
-# ==========================================
-
-# DBへのUSAGE権限
-resource "snowflake_grant_privileges_to_account_role" "training_db_usage" {
-  account_role_name = "FR_ANCHOR_DEMO_ROLE"
-  privileges        = ["USAGE"]
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = snowflake_database.training_db.name
-  }
-}
 
