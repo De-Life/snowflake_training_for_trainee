7f208d9748351b339428197bf596bb9e201b9123
ti2140 <143500765+ti2140@users.noreply.github.com>
2026-06-12 15:56:14 +0900

    Rename storage integration variable for clarity

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,7 +39,7 @@ variable "snowflake_role_name" {
   type = string
 }
 
-variable "storage_integration_name" {
+variable "snowflake_storage_integration_name" {
   type    = string
   default = "S3_INT"
 }
